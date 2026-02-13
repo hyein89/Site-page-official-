90a16d2b3a12fba8a4ed972d7fa06acbab61b0f9
hyein89 <bihontai@gmail.com>
2026-02-13 17:05:52 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+// config.js
+module.exports = {
+  // Link tujuan utama (Offer Link)
+  offerLink: "https://shopee.co.id/flash-sale", 
+  
+  // Waktu delay sebelum redirect (dalam milidetik, 3000 = 3 detik)
+  delayTime: 3000,
+  
+  // Teks yang muncul saat loading
+  loadingText: "Sedang memuat halaman promo...",
+  
+  // Nama situs untuk meta tag
+  siteName: "News Update Daily"
+};
